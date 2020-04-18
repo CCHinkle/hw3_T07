8b3dcfb60db5dd04f1bdc2fd75e92e73661b0515
KentWerner <63922460+KentWerner@users.noreply.github.com>
2020-04-18 17:35:04 -0500

    Update hw3_T07.cpp
    Base .cpp file from Liana Munoz

--- hw3_T07.cpp
+++ hw3_T07.cpp
@@ -1,7 +1,39 @@
-//******************************************************************************
-// Team # 07 CSCI/CMPE 1370 Spring2020 Homework # 3
-// Christopher Hinkle
-// Liana Munoz
-// ""DESCRIPTION""
-//
-//******************************************************************************
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include "hw3functions.h"
+using namespace std;
+
+int main (){
+    bool start = true;
+    char o,u;
+
+while (start){
+    showmenu();
+    cin >> 0;
+
+if (o=='A' || o=='a'){
+    addition ();
+}
+if (o=='S' || o=='s'){
+    subtraction (); 
+}
+if (o=='q' || o=='Q'){
+    quit ();
+}
+
+cout << "Would you like to try another operation? (y/n)" << '\n';
+cin >> u;
+'\n';
+
+if (u=='y' || u=='Y'){
+    start = true;
+    system ("CLS");
+    }
+else if (u!='y' || u!='Y'){
+    start = false;
+    quit();
+    }
+    return 0;
+    
+}
