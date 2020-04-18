ec7764f2c8c621354a49605f12e83a7f63efc717
CCHinkle <63796599+CCHinkle@users.noreply.github.com>
2020-04-18 02:25:15 -0500

    Introduce Code
    Riddled with errors. Will need to be fixed but most of the structure is fine.

--- hw3_T07.cpp
+++ hw3_T07.cpp
@@ -5,3 +5,42 @@
 // ""DESCRIPTION""
 //
 //******************************************************************************
+
+#include <iostream>
+using namespace std;
+
+#include "Fun.h"
+
+// get rid of _1 on switch if we only use 1
+int main(){
+    char choice[1];
+    bool switch_1 = 1;
+
+    //shows user options
+    showmenu();
+
+    while(switch_1 == 1){ // I'm not sure why this loop is terminating while switch_1 != 0
+
+        cin.get(choice, 2);
+    
+        if((choice[0] == 'A') || (choice[0] == 'a')){
+            !switch_1;
+            add();
+        }
+        else if( (choice[0] == 'S') || (choice[0] == 's') ){
+            !switch_1;
+            subtract();
+        }
+        else if( (choice[0] == 'Q') || (choice[0] == 'q') ){
+            cout << "Thank You!" << endl;
+            return 0;
+        }
+        else{
+            cout << "Please try again." << endl;
+        }
+    }
+    
+    // find a way to ask if the user wants to repeat
+
+    return 0;
+}
